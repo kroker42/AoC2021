f5e0b98cf412dbe78b3588f2f13d27bb5c9e7d07
kroker42 <git@frisch.se>
2021-12-04 05:45:03 +0000

    day 4 - mark_board()

--- main.py
+++ main.py
@@ -77,7 +77,8 @@ def day2():
 
     return time.time() - start_time, task1, task2
 
-# Day 3
+
+# Day 3 diagnostics
 
 def check_frequency(pos, data):
     frequency = 0
@@ -96,7 +97,9 @@ def scrub_list(common, pos, data):
                 keepers.append(i)
         elif not common:
             keepers.append(i)
+
     return [data[p] for p in keepers]
+
 
 def scrub(common, data):
     scrubbed_data = data.copy()
@@ -120,14 +123,15 @@ class Day3Test(unittest.TestCase):
             '11001',
             '00010',
             '01010']
+
     def test_check_frequency(self):
         self.assertEqual('1', check_frequency(0, self.data))
         self.assertEqual('0', check_frequency(1, self.data))
 
     def test_gamma(self):
         gamma = ''.join([check_frequency(i, self.data) for i in range(0, 5)])
-        self.assertEqual(22, int(gamma, 2))
         epsilon = ''.join('1' if x == '0' else '0' for x in gamma)
+        self.assertEqual(22, int(gamma, 2))
         self.assertEqual(9, int(epsilon, 2))
 
     def test_oxygen_scrub(self):
@@ -139,6 +143,7 @@ class Day3Test(unittest.TestCase):
         self.assertEqual(['00100', '01111', '00111', '00010', '01010'],
                          scrub_list(False, 0, self.data))
         self.assertEqual('01010', scrub(False, self.data))
+
 
 def day3():
     data = [line.strip() for line in open("day3input.txt")]
@@ -155,10 +160,82 @@ def day3():
 
     return time.time() - start_time, task1, task2
 
+# Day 4 - Squid Bingo
+
+def mark_board(num, board):
+    for i in range(0, len(board)):
+        board[i] = [-1 if x == num else x for x in board[i]]
+        if sum(board[0]) == -5:
+            return True
+
+    for i in range(0, len(board[0])):
+        if sum([x for x in [row[i] for row in board]]) == -5:
+            return True
+
+    return False
+
+
+def bingo(drawn, boards):
+    for d in drawn:
+        for board in boards:
+            if mark_board(d, board):
+                return board
+
+
+class Day4Test(unittest.TestCase):
+
+    data = [7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1]
+    boards = [
+        [[22, 13 ,17 ,11 , 0],
+        [8,  2 ,23 , 4, 24],
+        [21,  9, 14 ,16  ,7],
+        [6 ,10,  3 ,18  ,5],
+        [1 ,12, 20 ,15 ,19]],
+
+        [[3 ,15 , 0 , 2 ,22],
+        [9 ,18 ,13, 17  ,5],
+        [19,  8 , 7, 25 ,23],
+        [20, 11 ,10 ,24  ,4],
+        [14, 21 ,16 ,12  ,6]],
+
+        [[14, 21 ,17, 24 , 4],
+        [10 ,16 ,15 , 9 ,19],
+        [18 , 8 ,23 ,26 ,20],
+        [22 ,11, 13 , 6 , 5],
+        [2  ,0, 12 , 3 , 7]]
+    ]
+
+    def test_mark_board_line(self):
+        board = self.boards[0].copy()
+        self.assertFalse(mark_board(22, board))
+        self.assertEqual(-1, board[0][0])
+        mark_board(13, board)
+        mark_board(17, board)
+        mark_board(11, board)
+        self.assertTrue(mark_board(0, board))
+        self.assertEqual([-1] * 5, board[0])
+
+    def test_mark_board_line(self):
+        board = self.boards[0].copy()
+        mark_board(13, board)
+        mark_board(2, board)
+        mark_board(9, board)
+        mark_board(10, board)
+        self.assertTrue(mark_board(12, board))
+        self.assertEqual([-1] * 5, [row[1] for row in board])
+
+def day4():
+    data = [line.split() for line in open('day4input.txt')]
+    start_time = time.time()
+
+    task1 = None
+    task2 = None
+
+    return time.time() - start_time, task1, task2
+
 # Day
 
 class DayTest(unittest.TestCase):
-
 
     def test_(self):
         self.assertEqual(True, True)
@@ -172,6 +249,7 @@ def day():
     task2 = None
 
     return time.time() - start_time, task1, task2
+
 
 def run(day):
     run_time, task1, task2 = day()
@@ -188,7 +266,7 @@ def run_tests():
 
 if __name__ == '__main__':
     run_tests()
-    for i in range(1, 4):
+    for i in range(1, 5):
         run(eval("day" + str(i)))
 
 
